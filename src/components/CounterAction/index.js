a3bd58048f41ea7d428f961aef199d19d9350984
salsabilaathika-97 <salsathikar@gmail.com>
2022-08-27 10:49:42 +0800

    create counter components

---
+++ src/components/CounterAction/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CounterAction = () => {
+    return (
+        <div>
+            <button>+</button>
+            <button>-</button>
+            <button>Reset</button>
+        </div>
+    )
+}
+
+export default CounterAction;
---
+++ src/components/CounterResult/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CounterResult = () => {
+    return (
+        <div>
+            <h1>0</h1>
+        </div>
+    )
+}
+
+export default CounterResult;
